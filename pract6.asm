3c51aa34b43c04dde2f27346123fc215c2acf4bd
Mario Marinero <mariomarinero@gmail.com>
2009-04-21 23:08:06 +0200

    semiperimeter calculated, it is going to be easy

--- pract6.asm
+++ pract6.asm
@@ -6,10 +6,15 @@
 
 segment .data
 msgPedirDatos db "Introduzca los tres lados del triangulo\n",0
-fRecogerDatos db "%lf"
-fResultado db "Area triangulo: %lf\n",0 
+fRecogerDatos db "%lf%lf%lf"
+fResultado db "Area triangulo: %f",10,0 
 
 segment .bss
+
+a resq 1
+b resq 1
+c resq 1
+s resq 1 ;semiperimetro
 
 
 segment .text
@@ -19,6 +24,28 @@ extern printf, scanf
 pract6:
     enter 0,0
     pusha
+
+    push c
+    push b
+    push a
+    push fRecogerDatos
+    call scanf
+    add esp, 16
+
+    ;calculo semiperimetro
+    fld qword [a]
+    fadd qword [b]
+    fadd qword [c]
+    mov dword [s], 2 ; s aun no ha sido usada
+    fidiv dword [s]
+
+    ;imprimir resultado
+    fstp qword [a]
+    push dword [a+4]
+    push dword [a]
+    push fResultado
+    call printf
+    add esp, 12
 
     popa
     leave
