a9b93786f7c7c82b41ea7baa59f1a46a134f0a80
Mario Marinero <mariomarinero@gmail.com>
2009-11-19 01:56:51 +0100

    Esta vez si que se acaba solo queda el repaso de antes de entregar

--- check.bash
+++ check.bash
@@ -1,10 +1,10 @@
 #!/bin/bash
 for vfundido in `seq 0 255`
 do
-    ./fundido <<<`echo $vfundido` | egrep -o "[0-9]+$" >>tiempos.txt 
+    ./fundido <<<`echo $vfundido` | egrep -o "[0-9]+$" >>.tiempos.txt 
     if ! diff fundido.bmp fundidoc.bmp;
 	then echo "error con vfundido: $vfundido";
     fi
 done
 ./sumatiempo.py
-rm tiempos.txt
+rm .tiempos.txt
--- sumatiempo.py
+++ sumatiempo.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python
 flag = [1, 0]
-mmx, c = (0, 0)
-file = open("tiempos.txt","r")
+mmx, c = (0.0, 0.0)
+file = open(".tiempos.txt","r")
 for line in file.readlines():
     c = c + int(line)*flag[0]
     mmx = mmx + int(line)*flag[1]
     flag.reverse()
-print "c %i\nmmx %i\nganancia %f\n" % (c, mmx, float(c)/mmx)
+print "t.m C:\t%f us\nt.m MMX\t%f us\nC/MMX\t%f" % (c/256, mmx/256, c/mmx)
